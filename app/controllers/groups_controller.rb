71256cdbab5cd7f625155aab3747b523c5a4b04b
izuharayuichi <izuhara_yuichi@izuharayuuichinoMacBook-puro.local>
2018-10-18 13:53:51 +0900

    make_collection_check_bowes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,23 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    Group.create(group_params)
+      # redirect_to controller: :messages action: :index
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.permit(:name)
   end
 end
