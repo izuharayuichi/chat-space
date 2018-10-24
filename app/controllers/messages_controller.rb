0c1f14c36b728ee7668aaa245541e496cce82ff4
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-10-24 17:05:35 +0900

    4.messages#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,13 +8,13 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     # if @message.save
     #   respond_to do |farmat|
-    #     format.html(@group) { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
-    #     format.json { render json: @message }
+    #     format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+    #     format.json
     #   end
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
