ad39af3b2cd3fe401fbdca9f8a0d3615425d1b16
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-10-25 12:41:32 +0900

    7.done_method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,22 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="a-message">
+                  <div class="name-and-date">
+                    <p class="name-and-date__post-user">
+                      ${message.user.name}
+                    </p>
+                    <p class="name-and-date__post-date">
+                      ${format_posted_time(message.created_at)}
+                    </p>
+                    <% if message.content.present? %>
+                      ${message.content}
+                    <% end %>
+                    <% if message.image.present? %>
+                      ${message.image.url}
+                    <% end %>
+                  </div>
+                </div>`
+  }
   $('.js-form').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +28,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.textbox').val('')
     })
   });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
   json.text  @message.text
   json.user_id  @message.user.id
   json.group_id @json.group.id
-  json.user_name  @message.user.nickname
+  json.user_name  @message.user.name
