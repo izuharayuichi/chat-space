053f7650eb911c093af67d5821009ad919506653
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-10-29 13:49:05 +0900

    1.add_members

--- app/assets/javascripts/channels/index.js
+++ app/assets/javascripts/channels/index.js
@@ -1,13 +1,14 @@
 $(function(){
 
-  var search_list = $(".chat-group-form__search");
+  var search_list = $("#user-search-result");
+  var member_list = $("#chat-group-users");
 
   function appendName(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+                  <a class="user-search-add${ user.id } chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                 </div>`
-      search_list.append(html);
+    search_list.append(html);
   }
 
   function appendNoName(user){
@@ -17,7 +18,16 @@ $(function(){
     search_list.append(html);
   }
 
-  $('.chat-group-form__field--right').on('keyup', function(e){
+  function registerName(user){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user.id }'>
+                <input name='group[user_ids][]' type='hidden' value='${ user.id }'>
+                <p class='chat-group-user__name'>${ user.name }</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+    member_list.append(html);
+  }
+
+  $("#user-search-field").on('keyup', function(e){
     var input = $("#user-search-field").val();
 
     $.ajax({
@@ -28,10 +38,14 @@ $(function(){
     })
 
     .done(function(users){
-      $(".chat-group-user").remove();
+      $("#user-search-result").empty();
       if (users.length !== 0 ) {
         users.forEach(function(user){
           appendName(user);
+          $(`.user-search-add${ user.id }`).on('click', function(){
+            registerName(user)
+            $(this).parent().remove();
+          })
         });
       }
       else {
