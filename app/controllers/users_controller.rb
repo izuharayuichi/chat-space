40a4fb26e2005eb71b11ce0719f8bb7be6a88258
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-10-29 17:22:21 +0900

    edit_users_controller(where)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users_not_current = User.where.not(id: current_user.id)
-    @users = @users_not_current.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
