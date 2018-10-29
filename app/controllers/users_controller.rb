3b951dd115f40d6d1af39c45fd19c2b520b13e13
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-10-29 17:01:04 +0900

    edit_users_controller
    Delete limit
    Add where.not current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    @users_not_current = User.where.not(id: current_user.id)
+    @users = @users_not_current.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
