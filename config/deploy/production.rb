d5ba4a8a832303afb196d7eaf4388ea2d8c983ee
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-11-03 11:55:59 +0900

    Update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '<用意したElastic IP>', user: 'ec2-user', roles: %w{app db web}
+server '176.32.77.171', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
