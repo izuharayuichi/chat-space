5e59ece9a86da933b01017798bc933df97001935
izuharayuichi <y12i26r53@hotmail.co.jp>
2018-11-04 16:33:53 +0900

    edit_member_remove_remain

--- app/assets/javascripts/channels/index.js
+++ app/assets/javascripts/channels/index.js
@@ -22,7 +22,7 @@ $(function(){
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user.id }'>
                 <input name='group[user_ids][]' type='hidden' value='${ user.id }'>
                 <p class='chat-group-user__name'>${ user.name }</p>
-                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                <a class='user-search-remove${ user.id } chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
               </div>`
     member_list.append(html);
   }
@@ -46,8 +46,8 @@ $(function(){
           $(`.user-search-add${ user.id }`).on('click', function(){
             registerName(user)
             $(this).parent().remove();
-            $(`#chat-group-user-${ user.id }`).on('click', function(){
-              $(this).parent().empty();
+            $(`.user-search-remove${ user.id }`).on('click', function(){
+              $(this).parent().remove();
             })
           })
         })
